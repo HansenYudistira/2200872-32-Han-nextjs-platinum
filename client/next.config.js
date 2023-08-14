ef971728a9dc2a82f1c513c781a04cab334d8f31
agnes-septilia <agnes.septilia@gmail.com>
2023-08-14 21:14:58 +0700

    edit image tag, server URL

--- client/next.config.js
+++ client/next.config.js
@@ -3,7 +3,11 @@ module.exports = {
     unoptimized: true,
   },
   env: {
+<<<<<<< Updated upstream
     REACT_APP_BE_URL: 'http://localhost:5000'
+=======
+    REACT_APP_BE_URL: 'https://rose-creepy-haddock.cyclic.app'
+>>>>>>> Stashed changes
   },
   devIndicators: {
     buildActivity: false
